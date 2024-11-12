c150fa1ee16a0a7cdb07102baa163153ee228442
jonorl <39122230+jonorl@users.noreply.github.com>
2024-11-12 15:54:02 +0300

    Managed to get temperature to console

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,34 @@
 
 import "../css/style.css";
 
+// Global variables
+
+const api_key = 'Z5ESM8BZKP44Y6TQXX3ZX5VS4';
+const location = 'Kampala';
+const URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${api_key}&unitGroup=metric`;
+
 // Event Listeners
+
+// searchBtn.addEventListener("click", () => {
+
+function weather() {
+  getWeather();
+  async function getWeather() {
+    try {
+        const response = await fetch(
+            URL,
+            { mode: "cors" }
+          );
+      const weatherData = await response.json();
+      console.log(weatherData.days[0].temp);
+    } catch (err) {
+      err();
+    }
+  }
+}
+
+function err() {
+  console.log("Error");
+}
+
+weather();
