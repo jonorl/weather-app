ce7f673737cb8051a3e9703ac30029ae52d9abe4
jonorl <39122230+jonorl@users.noreply.github.com>
2024-11-13 11:12:42 +0300

    Added loader and city and temperature display

--- src/js/index.js
+++ src/js/index.js
@@ -4,32 +4,60 @@ import "../css/style.css";
 
 // Global variables
 
-const api_key = 'Z5ESM8BZKP44Y6TQXX3ZX5VS4';
-const location = 'Kampala';
-const URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${api_key}&unitGroup=metric`;
+const api_key = "Z5ESM8BZKP44Y6TQXX3ZX5VS4";
+let location;
+let unit;
+let URL;
+let temperature;
+const searchBtn = document.querySelector("#search");
+const loader = document.querySelector('.loader');
 
 // Event Listeners
 
-// searchBtn.addEventListener("click", () => {
+searchBtn.addEventListener("click", () => {
+  location = document.querySelector("#city").value;
+  unit = document.querySelector("#metric").checked ? "metric" : "us";
+  URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${api_key}&unitGroup=${unit}`;
+  weather();
+});
 
 function weather() {
   getWeather();
   async function getWeather() {
     try {
-        const response = await fetch(
-            URL,
-            { mode: "cors" }
-          );
+      loader.style.display = 'block';
+      const response = await fetch(URL, { mode: "cors" });
       const weatherData = await response.json();
+      loader.style.display = 'none';
+      temperature = weatherData.days[0].temp
       console.log(weatherData.days[0].temp);
-    } catch (err) {
+      renderTemp();
+    } catch (error) {
       err();
     }
   }
 }
 
 function err() {
-  console.log("Error");
+  alert("Error");
 }
 
-weather();
+function renderTemp() {
+  const resultsContainer = document.querySelector('.results')
+
+  // Remove children if any
+
+  while (resultsContainer.firstChild) {
+    resultsContainer.removeChild(resultsContainer.lastChild);
+  }
+
+  const cityNameDisplay = document.createElement("div");
+  cityNameDisplay.classList.add("cityNameDisplay");
+  cityNameDisplay.textContent = location;
+
+  const temperatureDisplay = document.createElement("div");
+  temperatureDisplay.classList.add('temperatureDisplay');
+  temperatureDisplay.textContent = temperature
+
+  resultsContainer.append(cityNameDisplay, temperatureDisplay);
+}
